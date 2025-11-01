3db9536d82a2488c8f4d1138442e1cc695a2e86d
valentinaromar1 <200642560@student.georgianc.on.ca>
2025-11-01 19:04:35 -0400

    added a way to get info
    added a way for info-pizza.php to get the info from index

--- assignment-1/info-pizza.php
+++ assignment-1/info-pizza.php
@@ -5,16 +5,26 @@
 		<title>papyrus pizza™</title>
 		<link rel="stylesheet" href="css/styles.css" />
 	</head>
+	<body>
+		<header>
+			<h1>©Papyruspizza</h1>
+		</header>
 	<main>
 	   <?php
 		
-	   $
+	   $name = $_GET["name"];
 	   
 	 
-	 
+	 	echo('<p>The Alert Settings you have chosen include the following notifications:</p><ul>');
+		
+		echo('</ul>');
 	 ?>
-	   </main>
 
-<footer>
-    <p><small>©Papyruspizza Inc. All rights reserved</small></p>
-</footer>
+	</main>
+
+	<footer>
+    	<p><small>©Papyruspizza Inc. All rights reserved</small></p>
+	</footer>
+
+	</body>
+</html>
