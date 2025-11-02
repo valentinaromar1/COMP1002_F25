72653a5d48381b6638aadfdff05b1b0be3c2b1a6
valentinaromar1 <200642560@student.georgianc.on.ca>
2025-11-01 20:46:15 -0400

    added more gets

--- assignment-1/info-pizza.php
+++ assignment-1/info-pizza.php
@@ -19,8 +19,28 @@
 	 	echo('<p>your order is being processed:</p><ul>');
 
 		$crusttype = $_GET["crusttype"];
+		$shape = $_GET["shape"];
+		$size = $_GET["size"];
+		$topping1 = $_GET["topping1"];
+		$topping2 = $_GET["topping2"];
+		$topping3 = $_GET["topping3"];	
 		
-		echo(.crusttype.);
+		$address = $_GET["address"];
+	    $phonenumber = $_GET["phone number"];
+		
+		echo('<p>crust type:'.$crusttype.'</p>');
+		echo('<p>shape:'.$shape.'</p>');
+		echo('<p>topping1:'.$toping1.'</p>');
+		echo('<p>topping2:'.$topping2.'</p>');
+		echo('<p>topping3:'.$topping3.'</p>');
+		echo('<p>address:'.$address.'</p>');
+		echo('<p>phonenumber:'.$phonenumber.'</p>');
+
+	
+		
+
+
+
 	 ?>
 
 	</main>
