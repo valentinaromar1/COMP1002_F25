c2edb18162ef4973527b52ec4db6741384a1a23d
valentinaromar1 <200642560@student.georgianc.on.ca>
2025-11-28 17:12:43 -0500

    almost finished lab-e
    i worked on lab-e but could not get it to work (i refuse to put it through AI)

---
+++ lesson-12/js/external.js
@@ -0,0 +1,1 @@
+//alert("enternal javaScript is running")
--- lesson-12/js/gallery.js
+++ lesson-12/js/gallery.js
@@ -4,31 +4,39 @@
 // IMPORTANT: Begin by adding a SCRIPT element to 'index.html' that refers to this EXTERNAL JavaScript File
 
 // STEP 1a: Wait until the DOM is ready to be manipulated
-
+document.addEventListener("DOMContentLoaded", function(){
+    //alert("DOM is ready to go");
 // STEP 2a: Grab the UL for the image gallery and assign it to the variable 'gallery'
-
+    const gallery = document.querySelector("#imagegallery");
+    //console.log(gallery);
 // STEP 2b: Create an array composed of all the A (anchor) elements inside that UL, now referred to as 'gallery'
-
+    const links = document.querySelectorAll("#imagegallery a");
+    //console.log(links);
 // STEP 2c: Create a loop that iterates through each item in the links array that we just created
-
-// STEP 3a: For each one of those A elements, attach an onclick attribute that calls an anonymous function
-
-// STEP 4a: For the link that was clicked, capture the value of the href attribute as a variable called 'source'
-
-// STEP 4b: Grab the IMG element with the id 'placeholder' and assign it to the variable of the same name
-
-// STEP 4c: Change the image file that the IMG element points to in the src attribute based on the link clicked
-
-// STEP 5a: Capture the title attribute of the link clicked and assign it as the value for the variable 'text'
-
-// STEP 5b: Grab the P element with the id 'description'
-
-// STEP 5c: Set the content of that paragraph to the value of the title attribute for the link clicked
-
-// STEP 3c: Cancel the default behaviour of each hyperlink (which is to follow the href)
-
-// STEP 3b: End the 'onclick' event anonymous function
-
-// STEP 2d: End the loop through the links array
-
+    for (let i = 0; i < links.length; i ++){
+        // STEP 3a: For each one of those A elements, attach an onclick attribute that calls an anonymous function
+        //console.log(links[i]);
+        links[i].addEventListener("click", function(event){
+                //console.log("the anchor clicked is" + event);
+                // STEP 4a: For the link that was clicked, capture the value of the href attribute as a variable called 'source'
+                let source = links[i].getAttribute("href");
+                //console.log(source);
+                // STEP 4b: Grab the IMG element with the id 'placeholder' and assign it to the variable of the same name
+                const placeholder = document.querySelector("#placeholder");
+                // STEP 4c: Change the image file that the IMG element points to in the src attribute based on the link clicked
+                placeholder.setAttribute("src", source);
+                // STEP 5a: Capture the title attribute of the link clicked and assign it as the value for the variable 'text'
+                let text = links[i].getAttribute("title");
+                console.log(text);
+                // STEP 5b: Grab the P element with the id 'description'
+                const description = document.querySelector("#description")
+                // STEP 5c: Set the content of that paragraph to the value of the title attribute for the link clicked
+                description.textContent = text;
+                // STEP 3c: Cancel the default behaviour of each hyperlink (which is to follow the href)
+                event.preventDefault();
+                // STEP 3b: End the 'onclick' event anonymous function
+            });
+        // STEP 2d: End the loop through the links array
+        };
 // STEP 1b: End the DOM ready function
+});
